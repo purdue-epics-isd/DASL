33c75b5bf16b13ebc05b2e636ab88872c3975185
pwoodling <pwoodlin@purdue.edu>
2018-03-21 14:13:20 -0400

    database

--- DASL/DASL/DASL/App.xaml.cs
+++ DASL/DASL/DASL/App.xaml.cs
@@ -2,18 +2,27 @@
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using SQLite;
+using System.IO
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace DASL
 {
 	public partial class App : Application
-	{
+	{ //path string for database
+        string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal),"dbTest.db3") 
         public App()
 		{
 			InitializeComponent();
 
 			SetMainPage();
+
+            var db = new SQLiteConnection(dbPath);
+
+            db.CreateTable<Word>();
+
 		}
+
 
 		public static void SetMainPage()
 		{
