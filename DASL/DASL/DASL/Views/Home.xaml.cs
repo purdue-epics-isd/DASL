bfa73511f65d053f00ad81ec4439f8aa0bc1e2d8
Caleb Withers <cawithers@gmail.com>
2018-03-28 18:39:23 -0400

    added updated xamarin.forms app

---
+++ DASL/DASL/DASL/Views/Home.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DASL.Models;
+using DASL.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DASL.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Home : ContentPage
+	{
+		public Home ()
+		{
+			InitializeComponent();
+
+            BindingContext = viewModel = new ItemsViewModel();
+        }
+	}
+}
