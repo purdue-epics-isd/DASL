172ea1b37bc454e01f600f10c26e7b6945ef2831
McWithers <cawithers@gmail.com>
2018-03-28 22:36:56 -0400

    Created the Xamarin Forms App
    Added the home page, word of the day and word of the week pages.

---
+++ DASL2.0/DASLv2/DASLv2/DASLv2/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DASLv2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        
+        async void OnEnglishSearch (object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                    "English Search",
+                    "Would you like to call ?",
+                    "Yes",
+                    "No"))
+            {
+            }
+        }
+        async void OnWOTD(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new WordPage("Word of the Day", "Hello There"));
+        }
+        async void OnWOTW(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new WordPage("Word of the Week", "Hello There"));
+        }
+    }
+}
---
+++ DASL2.0/DASLv2/DASLv2/DASLv2/WordPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DASLv2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WordPage : ContentPage
+	{
+		public WordPage ()
+		{
+            InitializeComponent ();
+		}
+        public WordPage(string pageName, string pageWord)
+        {
+            InitializeComponent();
+            WordPageTitle.Text = pageName;
+            WordPageWord.Text = pageWord;
+        }
+    }
+}
