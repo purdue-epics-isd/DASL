9309f41b24e6d0029ba75e7436bff0cad1b69b37
burkeg <burkeg@purdue.edu>
2018-04-06 16:47:50 -0400

    Added MVVM binding for category pages

--- DASL2.0/DASLv2/DASLv2/DASLv2/CategoryPage.xaml.cs
+++ DASL2.0/DASLv2/DASLv2/DASLv2/CategoryPage.xaml.cs
@@ -12,6 +12,78 @@ using Xamarin.Forms.Xaml;
 namespace DASLv2
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public class PageDataViewModel
+    {
+        public PageDataViewModel(Type type, string title, string description)
+        {
+            Type = type;
+            Title = title;
+            Description = description;
+        }
+
+        public Type Type { private set; get; }
+
+        public string Title { private set; get; }
+
+        public string Description { private set; get; }
+
+        static PageDataViewModel()
+        {
+            All = new List<PageDataViewModel>
+        {
+            // Part 1. Getting Started with XAML
+            new PageDataViewModel(typeof(CategoryPage), "Hello, XAML",
+                                  "Display a Label with many properties set"),
+
+            new PageDataViewModel(typeof(WordPage), "XAML + Code",
+                                  "Interact with a Slider and Button"),
+/*
+            // Part 2. Essential XAML Syntax
+            new PageDataViewModel(typeof(GridDemoPage), "Grid Demo",
+                                  "Explore XAML syntax with the Grid"),
+            new PageDataViewModel(typeof(AbsoluteDemoPage), "Absolute Demo",
+                                  "Explore XAML syntax with AbsoluteLayout"),
+
+            // Part 3. XAML Markup Extensions
+            new PageDataViewModel(typeof(SharedResourcesPage), "Shared Resources",
+                                  "Using resource dictionaries to share resources"),
+
+            new PageDataViewModel(typeof(StaticConstantsPage), "Static Constants",
+                                  "Using the x:Static markup extensions"),
+
+            new PageDataViewModel(typeof(RelativeLayoutPage), "Relative Layout",
+                                  "Explore XAML markup extensions"),
+
+            // Part 4. Data Binding Basics
+            new PageDataViewModel(typeof(SliderBindingsPage), "Slider Bindings",
+                                  "Bind properties of two views on the page"),
+
+            new PageDataViewModel(typeof(SliderTransformsPage), "Slider Transforms",
+                                  "Use Sliders with reverse bindings"),
+
+            new PageDataViewModel(typeof(ListViewDemoPage), "ListView Demo",
+                                  "Use a ListView with data bindings"),
+
+            // Part 5. From Data Bindings to MVVM
+            new PageDataViewModel(typeof(OneShotDateTimePage), "One-Shot DateTime",
+                                  "Obtain the current DateTime and display it"),
+
+            new PageDataViewModel(typeof(ClockPage), "Clock",
+                                  "Dynamically display the current time"),
+
+            new PageDataViewModel(typeof(HslColorScrollPage), "HSL Color Scroll",
+                                  "Use a view model to select HSL colors"),
+
+            new PageDataViewModel(typeof(KeypadPage), "Keypad",
+                                  "Use a view model for numeric keypad logic")
+  */      };
+        }
+
+
+        public static IList<PageDataViewModel> All { private set; get; }
+    }
+
     public partial class CategoryPage : ContentPage
     {
         //public static int i = 0; //Change to non static once backend is done.
@@ -24,7 +96,7 @@ namespace DASLv2
         {
             InitializeComponent();
             CategoryPageTitle.Text = name;
-            CategoryListView.ItemsSource = items;
+            //CategoryListView.ItemsSource = items;
             this.isWordPage = isWordPage;
             //i++;
         }
@@ -33,6 +105,18 @@ namespace DASLv2
         {
             DisplayAlert("Item Tapped", e.Item.ToString(), "Ok");
         }*/
+
+        private async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            (sender as ListView).SelectedItem = null;
+
+            if (args.SelectedItem != null)
+            {
+                PageDataViewModel pageData = args.SelectedItem as PageDataViewModel;
+                Page page = (Page)Activator.CreateInstance(pageData.Type);
+                await Navigation.PushAsync(page);
+            }
+        }
 
         async void CategorySelected(object sender, SelectedItemChangedEventArgs e)
         {
